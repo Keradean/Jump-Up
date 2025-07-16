161540a199ec884c7dcb00d37137d433ed5b1df6
Dennis De Col <dennis.decol@gmx.net>
2025-07-16 21:55:53 +0200

    Update Player
    Change to Player Input

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,29 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movement; 
-    [SerializeField] private float movementSpeed; 
-
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float leftLimit;
+    [SerializeField] private float rightLimit;
 
     private Rigidbody2D rb2D;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector2 moveInput;
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>(); 
+    }
+
+    
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb2D.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+        }
+    }
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        movement = Input.GetAxis("Horizontal") * movementSpeed;
-    }
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 velocity = rb2D.linearVelocity;
-        velocity.x = movement;
+        velocity.x = moveInput.x * movementSpeed; 
         rb2D.linearVelocity = velocity;
+    }
+
+    void LateUpdate()
+    {
+        if (transform.position.x < leftLimit)
+        {
+            transform.position = new Vector3(rightLimit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > rightLimit)
+        {
+            transform.position = new Vector3(leftLimit, transform.position.y, transform.position.z);
+        }
     }
 }
