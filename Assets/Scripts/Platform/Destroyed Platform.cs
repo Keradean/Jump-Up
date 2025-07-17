1d365d9d655a81287d1e61a366d15f8627aee391
Dennis De Col <dennis.decol@gmx.net>
2025-07-17 14:01:19 +0200

    Update Level
    Added two new Platform types
    Implemented functionality for DestroyedPlatform to break on player collision
    Adjusted level design for better difficulty and flow

---
+++ Assets/Scripts/Platform/Destroyed Platform.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public Rigidbody2D rb2D; 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.y <= 0f)
+        {
+            if (collision.collider.GetComponent<Rigidbody2D>())
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+}
